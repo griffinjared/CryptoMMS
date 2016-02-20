eae6a8c2030c513fd7e4a9d4139b4897c85f444d
sdrafahl <sdrafahl@iastate.edu>
2016-02-20 11:41:31 -0600

    wejkdhfgjakshdfio

--- app/src/main/java/com/regnum/cryptomms/Send.java
+++ app/src/main/java/com/regnum/cryptomms/Send.java
@@ -1,8 +1,13 @@
 package com.regnum.cryptomms;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -11,7 +16,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 /**
  * Created by shane on 2/20/16.
  */
-public class Send extends Activity {
+class Send extends Activity {
 
 
     /**
@@ -19,7 +24,7 @@ public class Send extends Activity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient mClient;
-
+    private EditText phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +34,8 @@ public class Send extends Activity {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         setContentView(R.layout.send);
         mClient = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        phone=(EditText) findViewById(R.id.phoneNum);
+
     }
 
     @Override
@@ -49,8 +56,8 @@ public class Send extends Activity {
                 Uri.parse("android-app://com.regnum.cryptomms/http/host/path")
         );
         AppIndex.AppIndexApi.start(mClient, viewAction);
-    }
 
+    }
     @Override
     public void onStop() {
         super.onStop();
@@ -70,4 +77,31 @@ public class Send extends Activity {
         AppIndex.AppIndexApi.end(mClient, viewAction);
         mClient.disconnect();
     }
+
+    public void sendMess(View view){
+        String ph = phone.getText().toString();
+        String mess = findViewById(R.id.message).toString();
+
+        if(ph.length()==10){
+            sendSMS(ph,mess);
+        }
+
+
+
+    }
+
+    public void sendSMS(String phoneNum,String message){
+        PendingIntent pi = PendingIntent.getActivity(this,0,new Intent(this,Send.class),0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNum,null,message,pi,null);
+
+
+
+    }
+
+
+
+
+
+
 }
