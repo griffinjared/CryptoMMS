7017e350f4ba9e3f168f5cb26f155fb796231abe
sdrafahl <sdrafahl@iastate.edu>
2016-02-20 14:32:55 -0600

    wejkdhfgjakshdfio

---
+++ app/src/main/java/com/regnum/cryptomms/Keys.java
@@ -0,0 +1,65 @@
+package com.regnum.cryptomms;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+
+/**
+ * Created by shane on 2/20/16.
+ */
+public class Keys extends ContentProvider {
+
+    private ArrayList<String> keys = new ArrayList<String>();
+
+    public Keys(){
+        keys=null;
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    public String getKey(int index){
+        return keys.get(index);
+    }
+    public void addKey(String key){
+        keys.add(key);
+    }
+
+
+
+
+}
